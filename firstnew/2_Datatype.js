03a62aa02fcf3cad632b2630450b24452a501553
sumit3245 <basantg499@gmail.com>
2025-10-30 11:22:03 +0000

    Datatype

---
+++ firstnew/2_Datatype.js
@@ -0,0 +1,10 @@
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => True/
+// null => standalong value
+// undefined =>
+//     symbol => unique
+//object
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object
