453261ccde0416233c2ba3621efe0efc69a892e7
sumit3245 <basantg499@gmail.com>
2025-10-31 10:32:25 +0000

    ConversionOperation

---
+++ firstnew/03_ConversionOperation.js
@@ -0,0 +1,29 @@
+let a = "null"
+let b = Number(a)
+// console.log(a)
+// console.log(b)
+// console.log(typeof b)
+// console.log(typeof a)
+
+
+
+
+// "" = string
+//"112" = strung
+//223 = number
+//null = object
+//undefined = undefined
+//sum3345 = NaN
+//null = if its is show in number its is a zero
+// undefine = undefine
+// number = a = 1 = true 
+
+let aa = 0
+let bb = Boolean(aa)
+console.log(aa)
+console.log(bb)
+console.log(typeof aa)
+console.log(typeof bb)
+// null = in Boolean = aa = type is object bb = false 
+//undefined = in boolean = aa = type is undefined bb = false 
+// 1 = is true 0 = is false 
